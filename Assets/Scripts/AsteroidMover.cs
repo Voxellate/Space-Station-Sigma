98f9e2c39db1ab2da90335f7933b10628f061e36
Voxellate <LukeDugdale@icloud.com>
2016-07-08 12:51:45 +1000

    Lighting and asteroid generation
    Added lighting, random asteroid generation and some other minor tweaks.

---
+++ Assets/Scripts/AsteroidMover.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidMover : MonoBehaviour
+{
+	public float speed;
+	public Vector3 direction;
+
+	void Start ()
+	{
+		GetComponent<Rigidbody>().velocity = direction * speed;
+	}
+}
---
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidSpawner : MonoBehaviour {
+
+	public Vector2 spawnOrigin;
+	public Vector3 spawnBounds;
+	public GameObject[] hazards;
+
+	void Start ()
+	{
+		StartCoroutine (SpawnAsteroids ());
+	}
+
+	IEnumerator SpawnAsteroids ()
+	{
+		while (true) {
+			GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+			Vector3 spawnPosition = new Vector3 (Random.Range(spawnOrigin.x, spawnOrigin.x + spawnBounds.x), Random.Range(0, spawnBounds.y), Random.Range(spawnOrigin.y, spawnOrigin.y + spawnBounds.z));
+			Quaternion spawnRotation = Quaternion.identity;
+			Instantiate (hazard, spawnPosition, spawnRotation);
+			yield return new WaitForSeconds (Random.Range(6,10));
+		}
+	}
+}
