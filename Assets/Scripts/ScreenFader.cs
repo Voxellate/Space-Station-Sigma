ffb30b4128bc77ed6a7a82152f1e51a0b9df66f0
Voxellate <LukeDugdale@icloud.com>
2016-07-09 00:00:27 +1000

    Added fade in
    Added a fade in effect and cleaned up the Assets folder. Also changed
    the asteroid rotate script.

---
+++ Assets/Scripts/ScreenFader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScreenFader : MonoBehaviour {
+
+	public Image fadeImage;
+	public float fadeSpeed = 0.25f;
+
+	void Start(){
+		fadeImage.color = Color.black;
+	}
+	void Update(){
+		fadeImage.color = Color.Lerp (fadeImage.color, Color.clear, fadeSpeed * Time.deltaTime);
+		}
+}
